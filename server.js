e4e7e969e83eb0a56de3daf0a5fa6042ec70bc93
Agent-006 <sagargope2001@gmail.com>
2026-06-04 18:17:15 +0530

    feat: initialize backend project with Express server and basic routes

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+// Created a server
+const app = express();
+
+// Creating a route
+app.get("/", (req, res) => {
+    res.send("Hello World! This is express");
+});
+
+// Creating a /about route
+app.get("/about", (req, res) => {
+    res.send("About Page");
+});
+
+// Started the server
+app.listen(3000, () => {
+    console.log("Server is running at http://localhosl:3000");
+});
